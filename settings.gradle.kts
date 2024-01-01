8ef8f59d1f2a5e21f545d4bd6877466b495a3940
saikesav-sai <saikesav67254@gmail.com>
2024-01-01 19:07:50 +0530

    created the project files

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TRINADH_ELECTRONICS_&_COMPUTER_SERVICING_CENTRE"
+include(":app")
+ 
