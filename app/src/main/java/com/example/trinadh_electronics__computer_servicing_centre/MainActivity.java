cc4d45fc7e8747c99e69adde1308e7ad54e8bf88
rowdy041 <sriram.laxmi3@gmail.com>
2024-01-08 23:06:53 +0530

    .

--- app/src/main/java/com/example/trinadh_electronics__computer_servicing_centre/MainActivity.java
+++ app/src/main/java/com/example/trinadh_electronics__computer_servicing_centre/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.trinadh_electronics__computer_servicing_centre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button3);
+        button = findViewById(R.id.button4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,INSERTDATA.class);
+                Intent intent1=new Intent(MainActivity.this,VIEWDATA.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
