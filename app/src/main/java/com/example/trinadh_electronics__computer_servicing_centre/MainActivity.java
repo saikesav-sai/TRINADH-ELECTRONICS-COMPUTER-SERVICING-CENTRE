1e543bb778ca1e24f1ea2924876c53b0df70ba87
rowdy041 <sriram.laxmi3@gmail.com>
2024-01-11 08:14:49 +0530

    second update on 11-01-2024 on 8:14am

--- app/src/main/java/com/example/trinadh_electronics__computer_servicing_centre/MainActivity.java
+++ app/src/main/java/com/example/trinadh_electronics__computer_servicing_centre/MainActivity.java
@@ -1,39 +1,33 @@
 package com.example.trinadh_electronics__computer_servicing_centre;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.content.Intent;
-import android.view.View;
-public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private Button button1;
 
+
+import android.view.View;
+import android.widget.ImageButton;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+ ImageButton imageButton;
+ ImageButton imageButton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
-
-        button = findViewById(R.id.button3);
-        button1 = findViewById(R.id.button4);
-
-        button.setOnClickListener (new View.OnClickListener() {
-            @Override
-            public void onClick(View V) {
-                Intent intent=new Intent(MainActivity.this,INSERTDATA.class);
-
-                startActivity(intent);
-
-            }
-        });
-button1.setOnClickListener(new View.OnClickListener() {
+imageButton = (ImageButton) findViewById(R.id.insertdata);
+imageButton.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View v) {
-        Intent intent=new Intent(MainActivity.this,VIEWDATA.class);
-
-        startActivity(intent);
+        Intent intent = new Intent(MainActivity.this, INSERTDATA.class);
     }
 });
-
+ imageButton1 = (ImageButton) findViewById(R.id.viewdata);
+ imageButton1.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View v) {
+         Intent intent = new Intent(MainActivity.this, VIEWDATA.class);
+     }
+ });
     }
 }
